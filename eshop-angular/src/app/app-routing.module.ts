628cde63f03eaa4f7005daca1542df2e41555df2
Haris Tse <haris.tsetsekas@gmail.com>
2022-09-13 14:23:07 +0300

    part2

---
+++ eshop-angular/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemsComponent } from './items/items.component';
+
+const routes: Routes = [
+  { path: '', component: ItemsComponent },
+  { path: 'items', component: ItemsComponent },
+  { path: 'items/:id', component: ItemDetailsComponent }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
